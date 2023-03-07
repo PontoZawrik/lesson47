0d5a9eadcd52b7835aa2ee10207f0a323b8f964b
= <=>
2023-03-07 18:26:03 +0300

    Change main.cpp file.

--- lesson47/main.cpp
+++ lesson47/main.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
+	int a, b;
 
+	cout << "Input your numbers: ";
+	cin >> a >> b;
+	 
+	cout << "Sum numbers: " << a + b << endl;
 
+	system("pause"); 
 	return 0;
 }
