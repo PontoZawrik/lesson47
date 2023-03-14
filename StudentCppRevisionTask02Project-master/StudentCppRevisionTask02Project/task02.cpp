2ef83618df22c2135d7fb6d6f76d06fc80bf92d2
= <=>
2023-03-14 17:25:17 +0300

    add logic program

--- StudentCppRevisionTask02Project-master/StudentCppRevisionTask02Project/task02.cpp
+++ StudentCppRevisionTask02Project-master/StudentCppRevisionTask02Project/task02.cpp
@@ -22,6 +22,5 @@
 */
 
 int task02(int seeker, int coin) {
-	
-	return 0;
+	return coin % seeker;
 }
--- StudentCppRevisionTask02Project-master/StudentCppRevisionTask02Project/taskX.cpp
+++ StudentCppRevisionTask02Project-master/StudentCppRevisionTask02Project/taskX.cpp
@@ -21,5 +21,5 @@
 */
 
 int taskX(int number) {
-	return 0;
+	return number % 2 == 1 ? number + 1 : number + 2;
 }
